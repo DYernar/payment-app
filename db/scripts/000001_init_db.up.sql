ecdab42d1b6dc532b6917a8cadde160e51950d86
DYernar <eduisebai.1@gmail.com>
2021-12-22 23:16:29 +0600

    db

--- db/scripts/000001_init_db.up.sql
+++ db/scripts/000001_init_db.up.sql
@@ -1,5 +1,5 @@
--- CREATE ROLE payment WITH LOGIN PASSWORD 'payment';
--- CREATE DATABASE payment_db;
+CREATE ROLE payment WITH LOGIN PASSWORD 'payment';
+CREATE DATABASE payment_db;
 
 CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
@@ -15,4 +15,14 @@ CREATE TABLE IF NOT EXISTS roles (
     id bigserial PRIMARY KEY,
     name role_name NOT NULL DEFAULT 'ROLE_USER',
     user_id integer UNIQUE REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS wallets (
+    id bigserial PRIMARY KEY,
+    name text NOT NULL,
+    number int UNIQUE NOT NULL CHECK (number >= 111111111111 AND number <= 999999999999),
+    balance double precision DEFAULT 0 CHECK (balance  >= 0),
+    iin text REFERENCES users(iin),
+    UNIQUE (number, iin),
+    UNIQUE (iin, name)
 );
